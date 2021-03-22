80e6bc3421973602e4ddff0cc48344b7261dd279
ipolynkina <d.e.m.p.i.l@yandex.ru>
2021-03-22 14:58:58 +0300

    err

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,5 +6,12 @@ using namespace Hello;
 int main(int argc, char *argv[]) {
   Speaker* speaker = new Speaker();
 
+	int a = 0;
+	int b = 5;
+	if (a == a)
+	{
+		b = 15;
+	}
+	
   speaker->sayHello();  
 }
